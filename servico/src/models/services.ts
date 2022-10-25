4de5242765ebb49fe176a2334f5faad7418d81cd
Higor <higormariano10@gmail.com>
2022-10-24 22:53:05 -0300

    feat.

---
+++ servico/src/models/services.ts
@@ -0,0 +1,24 @@
+import { MariaDBDataSource } from "./data_source";
+import {Projeto, Usuario} from "./model";
+
+export class Service{    
+    start(){       
+            MariaDBDataSource.initialize().then( ()=>{
+                console.log("Inicializada a fonte de dados...");
+            }).catch((err)=>{
+                console.error("Erro de inicialização da fonte de dados!!");
+            }) 
+    }
+    async insert(projeto: Projeto){
+        await MariaDBDataSource.manager.save(projeto);        
+    }
+    async listAll(){
+       let list = await MariaDBDataSource.manager.find(Projeto);
+       return list;
+    }
+    async listAl(){
+        let list = await MariaDBDataSource.manager.find(Usuario);
+        return list;
+     }
+}
+
